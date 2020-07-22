39e1e8ded382b9f19f571c12c021a8053a4259c0
Daniel Lindsley <daniel@toastdriven.com>
2020-07-21 21:44:19 -0500

    Extra config option for RtD.

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,8 @@ extensions = [
     "sphinxcontrib.napoleon",
 ]
 
+master_doc = "index"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
